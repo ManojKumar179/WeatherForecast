fd9b6f140a07db1251cc9e27a70613677444c116
ManojKumar <manoj.cse76@gmail.com>
2023-10-23 10:46:14 +0530

    updated gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/WeatherForecast/',
+  base: '/WeatherForecast',
   plugins: [react()],
   test: {
     globals: true,
